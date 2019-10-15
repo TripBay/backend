57d76930c802e4f2b03e288ed75a11ac59783038
Janrey Maligro <malricjan25@mail.com>
2019-10-15 14:44:26 +0800

    s

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -15,11 +15,9 @@ class Cors
      */
     public function handle($request, Closure $next)
     {
-        $response = $next($request);
-        $response->header('Access-Control-Allow-Origin', '*');
-        $response->header('Access-Control-Allow-Methods', 'HEAD, GET, POST, PUT, DELETE');
-        $response->header('Access-Control-Allow-Headers', $request->header('Access-Control-Request-Headers'));
-        $response->header('Access-Control-Allow-Credentials', 'true');
-        return $response;
+        return $next($request)
+        ->header('Access-Control-Allow-Origin', '*')
+        ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+        ->header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, X-Token-Auth, Authorization');
     }
 }
